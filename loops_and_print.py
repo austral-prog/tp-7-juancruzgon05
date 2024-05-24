00d200d4adebe49f3784bb03d07c1cc9c4400fb2
juancruzgon05 <162723271+juancruzgon05@users.noreply.github.com>
2024-05-24 11:42:15 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,14 +1,14 @@
 def enumerate_list(list):
     nuevalista=[]
 	for index,palabra in enumerate(list):
-		if palabra:
-			nuevalista +=[f"{len(nuevalista)}. {palabra}"]
+		if palabra != "":
+			nuevalista.append(f"{len(nuevalista)}. {palabra}")
 	return nuevalista
 
 
 def enumerate_backwards(list):
     nuevalista=[]
 	for index,palabra in enumerate(list):
-		if palabra:
-			nuevalista +=[f"{len(nuevalista)}. {palabra[::-1]}"]
+		if palabra != "":
+			nuevalista.append(f"{len(nuevalista)}. {palabra[::-1]}")
 	return nuevalista
