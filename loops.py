90f4067fb0c15c9363ea7d0e0b5af8f0ce2df210
juancruzgon05 <162723271+juancruzgon05@users.noreply.github.com>
2024-05-31 15:32:18 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,35 @@
 def index_of_by_index(word, list, index):
-    return -1
-
+    for posicion, elemento in enumerate(list):
+        if posicion >= index:
+            if elemento == word:
+                return posicion
+    else:
+        return -1
 
 def index_of_empty(list):
+    for posicion,elemento in enumerate(list):
+        if elemento == "":
+            return posicion
     return -1
-
 
 def index_of(word, list):
-    return -1
-
+    for indice, elemento in enumerate(list):
+        if elemento == word:
+            return indice
+    else:
+        return -1
 
 def put(word, list):
+    for posicion in range(0, len(list)):
+        if list[posicion] == "":
+            list[posicion] = word
+            return posicion
     return -1
-
 
 def remove(word, list):
-    return -1
+    var = 0
+    for posicion,elemento in enumerate(list):
+        if elemento == word:
+            list[posicion] = ""
+            var = var + 1
+    return var
